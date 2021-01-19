cde02ef0708774e36083d84690efec67a711d576
Rajan Agaskar <ragaskar@gmail.com>
2021-01-19 14:07:30 -0800

    Add ability to filter results based on various attributes.

--- models/vuln_aggregator.rb
+++ models/vuln_aggregator.rb
@@ -1,17 +1,31 @@
+require 'Set'
 class VulnAggregator
+  include Enumerable
   SEVERITY_TYPES = ['catastrophic', 'critical', 'high', 'medium', 'low', 'unknown']
 
   class SeverityTypeError < StandardError
   end
 
   def initialize()
-    @results = {}
-    SEVERITY_TYPES.each { |type| @results[type] = [] }
+    @all = []
+    @by_severity = {}
+    @by_image = {}
+    @by_target = {}
+    @images = ::Set.new
+    @targets = ::Set.new
+    SEVERITY_TYPES.each { |type| @by_severity[type] = [] }
   end
 
   def add(vuln)
-    raise SeverityTypeError.new("Unknown Severity Type #{vuln.severity}") unless @results.has_key?(vuln.severity)
-    @results[vuln.severity].push(vuln)
+    raise SeverityTypeError.new("Unknown Severity Type #{vuln.severity}") unless @by_severity.has_key?(vuln.severity)
+    @all.push(vuln)
+    @by_severity[vuln.severity].push(vuln)
+    @by_image[vuln.image] ||= []
+    @by_image[vuln.image].push(vuln)
+    @by_target[vuln.target] ||= []
+    @by_target[vuln.target].push(vuln)
+    @images.add(vuln.image)
+    @targets.add(vuln.target)
   end
 
   def total_vuln_count
@@ -20,8 +34,42 @@ class VulnAggregator
 
   SEVERITY_TYPES.each do |type|
     define_method "#{type}_count" do
-      @results[type].count
+      @by_severity[type].count
     end
+  end
+
+  def by_severity(severity)
+    subset = self.class.new()
+    @by_severity[severity].map { |v| subset.add(v) }
+    subset
+  end
+
+  def by_image(image)
+    subset = self.class.new()
+    @by_image[image].map { |v| subset.add(v) }
+    subset
+  end
+
+  def by_target(target)
+    subset = self.class.new()
+    @by_target[target].map { |v| subset.add(v) }
+    subset
+  end
+
+  def images
+    @images
+  end
+
+  def targets
+    @targets
+  end
+
+  def severities
+    SEVERITY_TYPES
+  end
+
+  def each(&block)
+    @all.each(&block)
   end
 
 end
--- spec/models/vuln_aggregator_spec.rb
+++ spec/models/vuln_aggregator_spec.rb
@@ -3,8 +3,8 @@ require_relative '../../models/vuln_aggregator'
 
 RSpec.describe VulnAggregator, "#severity" do
 
-  def vuln(severity = "low")
-    double(:vuln, severity: severity)
+  def vuln(severity = "low", image = "kube-proxy:1.2", target = "someregistry.example.org/kube-proxy:1.2")
+    double(:vuln, severity: severity, image: image, target: target)
   end
 
   context "when a vulnerability with unknown severity type is added" do
@@ -38,4 +38,118 @@ RSpec.describe VulnAggregator, "#severity" do
       expect(aggregator.low_count).to eq(1)
       expect(aggregator.unknown_count).to eq(1)
   end
+
+  it "can return 'subsets' (vuln aggregators) by severity" do
+    vuln1 = vuln("critical", "foo:1.2")
+    vuln2 = vuln("critical", "bar:3.4")
+    vuln3 = vuln("high", "baz:5.6")
+
+    aggregator = VulnAggregator.new
+    aggregator.add(vuln1)
+    aggregator.add(vuln2)
+    aggregator.add(vuln3)
+
+    results = aggregator.by_severity("critical")
+    expect(results).to include(vuln1)
+    expect(results).to include(vuln2)
+    expect(results).not_to include(vuln3)
+    expect(results.critical_count).to eq(2)
+    expect(results.high_count).to eq(0)
+  end
+
+  it "returns a subset by severity even if no vulns with that severity exist" do
+    vuln1 = vuln("critical", "foo:1.2")
+    vuln2 = vuln("critical", "bar:3.4")
+    vuln3 = vuln("high", "baz:5.6")
+
+    aggregator = VulnAggregator.new
+    aggregator.add(vuln1)
+    aggregator.add(vuln2)
+    aggregator.add(vuln3)
+
+    results = aggregator.by_severity("low")
+    expect(results.total_vuln_count).to eq(0)
+  end
+
+  it "can return 'subsets' (vuln aggregators) by image" do
+    vuln1 = vuln("critical", "foo:1.2")
+    vuln2 = vuln("critical", "foo:1.2")
+    vuln3 = vuln("high", "foo:1.2")
+    vuln4 = vuln("low", "bar:3.4")
+
+    aggregator = VulnAggregator.new
+    aggregator.add(vuln1)
+    aggregator.add(vuln2)
+    aggregator.add(vuln3)
+    aggregator.add(vuln4)
+
+    results = aggregator.by_image("foo:1.2")
+    expect(results).to include(vuln1)
+    expect(results).to include(vuln2)
+    expect(results).to include(vuln3)
+    expect(results).not_to include(vuln4)
+    expect(results.critical_count).to eq(2)
+    expect(results.high_count).to eq(1)
+    expect(results.low_count).to eq(0)
+  end
+
+  it "returns a list of images" do
+    vuln1 = vuln("critical", "foo:1.2")
+    vuln2 = vuln("critical", "bar:3.4")
+    vuln3 = vuln("high", "baz:5.6")
+    aggregator = VulnAggregator.new
+
+    aggregator.add(vuln1)
+    aggregator.add(vuln2)
+    aggregator.add(vuln3)
+
+    results = aggregator.images
+    expect(results).to include("foo:1.2")
+    expect(results).to include("bar:3.4")
+    expect(results).to include("baz:5.6")
+  end
+
+  it "can return 'subsets' (vuln aggregators) by target" do
+    vuln1 = vuln("critical", "foo:1.2", "target1")
+    vuln2 = vuln("critical", "foo:1.2", "target1")
+    vuln3 = vuln("high", "foo:1.2", "target1")
+    vuln4 = vuln("low", "bar:3.4", "target2")
+
+    aggregator = VulnAggregator.new
+    aggregator.add(vuln1)
+    aggregator.add(vuln2)
+    aggregator.add(vuln3)
+    aggregator.add(vuln4)
+
+    results = aggregator.by_target("target1")
+    expect(results).to include(vuln1)
+    expect(results).to include(vuln2)
+    expect(results).to include(vuln3)
+    expect(results).not_to include(vuln4)
+    expect(results.critical_count).to eq(2)
+    expect(results.high_count).to eq(1)
+    expect(results.low_count).to eq(0)
+  end
+
+  it "returns a list of targets" do
+    vuln1 = vuln("critical", "foo:1.2", "target1")
+    vuln2 = vuln("critical", "foo:1.2", "target2")
+    vuln3 = vuln("low", "bar:3.4", "target3")
+    aggregator = VulnAggregator.new
+
+    aggregator.add(vuln1)
+    aggregator.add(vuln2)
+    aggregator.add(vuln3)
+
+    results = aggregator.targets
+    expect(results).to include("target1")
+    expect(results).to include("target2")
+    expect(results).to include("target3")
+  end
+
+  it "returns severity types" do
+    aggregator = VulnAggregator.new
+    expect(aggregator.severities).to eq(['catastrophic', 'critical', 'high', 'medium', 'low', 'unknown'])
+  end
+
 end
