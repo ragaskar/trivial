9fef4bb67725665bcf10af9e874f7bb032c1478b
Rajan Agaskar <ragaskar@gmail.com>
2021-01-22 15:05:11 -0800

    Improve naive result sorting loop
    - No need to nest so deeply, can do a single pass to sort.
    - Technically this result sort doesn't belong in a renderer, maybe later move to another
      class and test.

--- models/renderer.rb
+++ models/renderer.rb
@@ -6,27 +6,29 @@ class Renderer
   def text_report(severities)
     summary = []
     severities.each do |severity|
-      severity_results = @vuln_aggregator.by_severity(severity)
-      if severity_results.total_vuln_count == 0
+      severity_results =  grouped_result_set(severity)
+      if severity_results.length == 0
         summary.push("No #{severity} vulnerabilities found")
         next
       else
         summary.push("\n\n>>> #{severity} vulnerabilities\n\n".upcase)
-        images = severity_results.images
-        images.each do |image|
-          summary.push("IMAGE: #{image}\n")
-          image_results = severity_results.by_image(image)
-          targets = image_results.targets
-          targets.each do |target|
-            target_results = image_results.by_target(target)
-            summary.push("\tTarget: #{target}\n")
-            target_results.each do |target_vuln|
-              summary.push("\t  #{target_vuln.vulnerability_id} #{target_vuln.package_name} #{target_vuln.title} (Score #{target_vuln.score})")
-              summary.push("\t  #{target_vuln.url}")
-              summary.push("\t  #{target_vuln.description}")
-              summary.push("\n")
-            end
+        image = nil
+        target = nil
+
+        severity_results.each do |result|
+          if image != result.image
+            image = result.image
+            summary.push("IMAGE: #{image}\n")
+            target = nil
           end
+          if target != result.target
+            target = result.target
+            summary.push("\tTarget: #{target}\n")
+          end
+          summary.push("\t  #{result.vulnerability_id} #{result.package_name} #{result.title} (Score #{result.score})")
+          summary.push("\t  #{result.url}")
+          summary.push("\t  #{result.description}")
+          summary.push("\n")
         end
       end
     end
@@ -38,5 +40,12 @@ class Renderer
     string += "Unknown: #{@vuln_aggregator.unknown_count}"
     summary.push(string)
     summary.join("\n")
+  end
+
+  private
+  def grouped_result_set(severity)
+    by_severity_vulns = @vuln_aggregator.by_severity(severity)
+    #sort by image + target
+    by_severity_vulns.sort_by { |v| [v.image, v.target] }
   end
 end
